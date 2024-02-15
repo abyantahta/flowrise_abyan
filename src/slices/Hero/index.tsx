dc0866ee6df27050b9767a5ad94e9a30bc2d4ea9
Abyantahta <abyantahta2002@gmail.com>
2024-02-15 22:10:33 +0700

    feat: Add variation to Hero Slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -21,28 +21,61 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <Bounded as = "section"
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <div className="flex items-center flex-col bg-slate-300 ">
-        <PrismicRichText field={slice.primary.heading} components={{
-          heading1: ({children})=>(
-            <Heading as="h1" size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">{children}
-            </Heading>
-          )
-        }} />
-        <PrismicRichText field={slice.primary.body} components={{
-          paragraph: ({children}) =>(
-            <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 max-w-2xl">{children}</p>
-          )
-        }} />
-        <Button field={slice.primary.button_link} className="">
-        {slice.primary.button_text}
-        </Button>
-        <PrismicNextImage field={slice.primary.image} className="drop-shadow-md max-w-4xl w-full mt-12"/>
-      </div>
-    </Bounded>
+
+    
+    <>
+    {slice.variation == 'default' && (
+      <Bounded as = "section"
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+      >
+        <div className="flex items-center flex-col ">
+          <PrismicRichText field={slice.primary.heading} components={{
+            heading1: ({children})=>(
+              <Heading as="h1" size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0 text-center">{children}
+              </Heading>
+            )
+          }} />
+          <PrismicRichText field={slice.primary.body} components={{
+            paragraph: ({children}) =>(
+              <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 max-w-2xl">{children}</p>
+            )
+          }} />
+          <Button field={slice.primary.button_link} className="">
+          {slice.primary.button_text}
+          </Button>
+          <PrismicNextImage field={slice.primary.image} className="drop-shadow-md max-w-4xl w-full mt-12"/>
+        </div>
+      </Bounded>
+    )}
+
+    {slice.variation == 'horizontal' && (
+      <Bounded as = "section"
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+      >
+        <div className="flex flex-col md:flex-row md:flex-auto gap-12 ">
+          <div className="py-4">
+            <PrismicRichText field={slice.primary.heading} components={{
+              heading1: ({children})=>(
+                <Heading as="h1" size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0    text-center md:text-left ">{children}
+                </Heading>
+              )
+            }} />
+            <PrismicRichText field={slice.primary.body} components={{
+              paragraph: ({children}) =>(
+                <p className="text-2xl  font-normal leading-10 font-body text-slate-600 mb-4 max-w-2xl text-justify">{children}</p>
+              )
+            }} />
+            <Button field={slice.primary.button_link} className="mx-auto md:ml-0">
+            {slice.primary.button_text}
+            </Button>
+          </div>
+          <PrismicNextImage field={slice.primary.image} className="drop-shadow-md max-w-4xl w-full flex-grow md:w-1/2 rounded-xl"/>
+        </div>
+      </Bounded>
+    )}
+    </>
   );
 };
 
