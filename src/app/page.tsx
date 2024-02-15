e6aa737d50b2f0768dd89ef0b0d712c74d1e4482
Abyantahta <abyantahta2002@gmail.com>
2024-02-15 13:08:43 +0700

    feat: Add snapshot to heroslice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
+import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage");
-return <div className="bg-slate-500">it works</div>
+// return <div className="bg-slate-500">it works</div>
   return <SliceZone slices={page.data.slices} components={components} />;
 }
 
